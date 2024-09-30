73130ac3f1e82b59f1cac9a892c549c46a7cef2d
magisterblaman <hannes.blaman@lbs.se>
2024-09-30 13:59:05 +0200

    En parameter -> ett returvärde

--- AW23_PRRPRR01_08_Konverteringsalgoritmer/Program.cs
+++ AW23_PRRPRR01_08_Konverteringsalgoritmer/Program.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace AW23_PRRPRR01_08_Konverteringsalgoritmer {
 	internal class Program {
+		static double ConvertMetersToKilometers(int m) {
+			return m / 1000.0;
+		}
 		static void Main(string[] args) {
+			Console.WriteLine(ConvertMetersToKilometers(953));
 		}
 	}
 }
